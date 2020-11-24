2cc942c4dc076a2ab415cc0cf3aa54c93d2dd167
Gubbaadithya <70189785+Gubbaadithya@users.noreply.github.com>
2020-11-24 09:11:54 +0530

    Add files via upload

---
+++ L-12 Assignment Selectionsort iterative.py
@@ -0,0 +1,12 @@
+# Selection sort recursive
+# Function for selection sort
+def selectionsort(ar,i,n):
+    min_i=i
+    for j in range(i+1,n):
+        if ar[j]<ar[min_i]: min_i=j
+    ar[i],ar[min_i]=ar[min_i],ar[i]
+    if i+1<n: selectionsort(ar,i+1,n)
+
+ar=list(map(int,input("Enter elements to sort: ").split()))
+selectionsort(ar,0,len(ar))
+print("Selection sort : ", ar)
